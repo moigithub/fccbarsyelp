a84cc003aac97d78da2792349309aa225c59152c
moigithub <mcarlosman@gmail.com>
2015-08-11 18:13:34 -0500

    Added places server api

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/places', require('./api/place'));
   app.use('/api/data', require('./api/data'));
   //app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
